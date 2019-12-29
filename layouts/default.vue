6ad8708287ff504215e473d17afcb2aa20b4a69e
Mindas <mindaugas.lickus@webelevate.ie>
2019-12-29 21:53:57 +0000

    transition in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,61 @@
 <template>
   <div>
     <app-navigation />
+    <svg
+      :class="{ active: page === 'about' }"
+      xmlns="http://www.w3.org/2000/svg"
+      width="200"
+      height="200"
+      viewBox="0 0 447 442"
+    >
+      <!-- we use the transition group component, we need a g tag because it’s SVG-->
+      <transition-group name="list" tag="g">
+        <rect
+          ref="rect"
+          key="rect"
+          class="items rect"
+          width="171"
+          height="171"
+        />
+        <circle
+          id="profile"
+          key="circ"
+          class="items circ"
+          cx="382"
+          cy="203"
+          r="65"
+        />
+        <g id="text" key="text" class="items text">
+          <rect x="56" y="225" width="226" height="16" />
+          <rect x="56" y="252" width="226" height="16" />
+          <rect x="56" y="280" width="226" height="16" />
+        </g>
+        <rect
+          id="footer"
+          key="footer"
+          class="items footer"
+          y="423"
+          width="155"
+          height="19"
+          rx="9.5"
+          ry="9.5"
+        />
+      </transition-group>
+    </svg>
+    Click this ><nuxt-link to="/about">About</nuxt-link>
     <nuxt />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import AppNavigation from '~/components/AppNavigation.vue'
 
 export default {
   components: {
     AppNavigation
-  }
+  },
+  computed: mapState(['page'])
 }
 </script>
 
@@ -33,6 +77,7 @@ export default {
   .text {
     transform: rotate(90deg) scaleX(0.08) translate3d(-300px, -35px, 0);
     color: #e63946;
+    fill: aqua;
   }
   .footer {
     transform: translateX(100px, 0, 0);
